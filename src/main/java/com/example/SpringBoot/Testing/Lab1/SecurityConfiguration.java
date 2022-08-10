c1646ae23327bd0f837faef22c098514fae6e6e0
Paxton Edgar <paxtonedgar3@gmail.com>
2022-08-10 17:48:00 -0400

    init push

--- src/main/java/com/example/SpringBoot/Testing/Lab1/SecurityConfiguration.java
+++ src/main/java/com/example/SpringBoot/Testing/Lab1/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,18 +20,30 @@ public class SecurityConfiguration{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated().and().formLogin().and().logout();
-        return http.build();}
+                .antMatchers()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout();
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailService = new InMemoryUserDetailsManager();
 
-        String username = "thomas";
+        String username = "paxton";
         String password = "1234";
         UserDetails thisUser = User.withUsername(username)
                 .password(passwordEncoder().encode(password))
-                .authorities("read")
+                .authorities("admin")
                 .build();
 
         userDetailService.createUser(thisUser);
